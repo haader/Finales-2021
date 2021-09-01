ade9d83f78d8e716b8006a3edb95a2734f2411fc
haader <88641541+haader@users.noreply.github.com>
2021-09-01 11:52:44 -0300

    Add files via upload

---
+++ qb.js
@@ -0,0 +1,77 @@
+clic=true;
+function hover(){
+    
+
+if (clic==true){
+    document.getElementById("unidad1c").style.display="grid";
+    clic=false;
+}else{
+    document.getElementById("unidad1c").style.display="none";
+    clic=true;
+}
+
+};
+
+function hover2(){
+    if (clic==true){
+        document.getElementById("unidad2c").style.display="grid";
+        clic=false;
+    }else{
+        document.getElementById("unidad2c").style.display="none";
+        clic=true
+    }
+    
+};
+
+function hover3(){
+    if (clic==true){
+        document.getElementById("unidad3c").style.display="grid";
+        clic=false;
+    }else{
+        document.getElementById("unidad3c").style.display="none";
+        clic=true
+    }
+    
+};
+
+function hover4(){
+    if (clic==true){
+        document.getElementById("unidad4c").style.display="grid";
+        clic=false;
+    }else{
+        document.getElementById("unidad4c").style.display="none";
+        clic=true
+    }
+    
+};
+function hover5(){
+    if (clic==true){
+        document.getElementById("unidad5c").style.display="grid";
+        clic=false;
+    }else{
+        document.getElementById("unidad5c").style.display="none";
+        clic=true
+    }
+        
+};
+function hover6(){
+    if (clic==true){
+        document.getElementById("unidad6c").style.display="grid";
+        clic=false;
+    }else{
+        document.getElementById("unidad6c").style.display="none";
+        clic=true
+    }
+    
+};
+
+function hoverb(){
+    if (clic==true){
+        document.getElementById("bibliografiac").style.display="grid";
+        clic=false;
+    }else{
+        document.getElementById("bibliografiac").style.display="none";
+        clic=true
+    }
+    
+};
